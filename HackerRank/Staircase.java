a1d08c2e4deef803a6e6b4c34409e3319f9c9257
KirtanManek <21010101123@darshan.ac.in>
2022-07-27 22:11:32 +0530

    Create Staircase.java

---
+++ HackerRank/Staircase.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n) {
+    // Write your code here
+        for (int i = 0; i < n; i++){
+            for (int j = 2; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++){
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
+
+public class Staircase {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.staircase(n);
+
+        bufferedReader.close();
+    }
+}
