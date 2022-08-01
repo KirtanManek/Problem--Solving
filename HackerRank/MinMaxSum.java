170e4e02c7de8ccf1acac92c6ff2348e96d631f7
Dhruv Karavadiya <94845676+DhruvKaravadiya@users.noreply.github.com>
2022-08-01 11:29:34 +0530

    Update MinMaxSum.java

--- HackerRank/MinMaxSum.java
+++ HackerRank/MinMaxSum.java
@@ -54,3 +54,24 @@ public class MinMaxSum {
         bufferedReader.close();
     }
 }
+
+
+/*
+public class Solution{
+    public static void main(String[] args) {
+    // W rite your code here
+    Scanner sc = new Scanner(System.in);
+    //BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    String a[]=sc.nextLine().split(" ");
+    long temp = 0 ;
+    long sum=0;
+    Arrays.sort(a);
+    for(int k = 0 ; k < a.length ; k++){
+        sum+=Long.parseLong(a[k]);
+    }
+    System.out.println((sum-Long.parseLong(a[a.length-1]))+" "+(sum-Long.parseLong(a[0])));
+    
+        
+    }
+}
+*/
