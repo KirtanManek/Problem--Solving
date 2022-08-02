8c321d1d0acba8d175d909cadbf9b00849f4f2c4
KirtanManek <manekkirtan@gmail.com>
2022-08-02 11:59:21 +0530

    Update GradingStudents.java

--- HackerRank/GradingStudents.java
+++ HackerRank/GradingStudents.java
@@ -27,7 +27,7 @@ class Result {
 }
 
 public class GradingStudents {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
