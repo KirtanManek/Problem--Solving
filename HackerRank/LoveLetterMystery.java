655180b3cf9e3c64feb4ee5570ba862e310c2c0e
KirtanManek <manekkirtan@gmail.com>
2022-08-04 11:44:47 +0530

    Create LoveLetterMystery.java

---
+++ HackerRank/LoveLetterMystery.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'theLoveLetterMystery' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int theLoveLetterMystery(String s) {
+    // Write your code here
+        int i = 0, j = s.length() - 1, count = 0;
+        // String[] s1 = s.split("");
+        while(i<=j){
+            if(s.charAt(i) != s.charAt(j)){
+                count += Math.abs((int)s.charAt(i) - (int)s.charAt(j));
+            }
+        i++;
+        j--;
+            
+        }
+        return count;
+    }
+
+}
+
+public class LoveLetterMystery {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.theLoveLetterMystery(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
