6c0237574979e5bd61c47837bbd981f3f39dbf71
KirtanManek <manekkirtan@gmail.com>
2022-08-04 12:34:33 +0530

    Create HackerRankInAString.java

---
+++ HackerRank/HackerRankInAString.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+    // Write your code here
+        String hr = "hackerrank";
+        int count = 0;
+        int i = 0, j = 0;
+        while (i < hr.length()){
+            while (j < s.length()){
+              if (hr.charAt(i) == s.charAt(j)){
+                  count++;
+                  j++;
+                  break;
+              }
+              j++;
+            }
+            i++;
+        }
+        if (count == hr.length()) return "YES";
+        else return "NO";
+    }
+
+}
+
+public class HackerRankInAString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.hackerrankInString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
