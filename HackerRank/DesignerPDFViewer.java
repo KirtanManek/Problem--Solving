663817591c79a626b2b316664cc1508182931e93
KirtanManek <manekkirtan@gmail.com>
2022-09-03 16:21:06 +0530

    Create DesignerPDFViewer.java

---
+++ HackerRank/DesignerPDFViewer.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int boxSize(List<Integer> h, String word) {
+    // Write your code here
+        int max = 0;
+        for(int i = 0; i < word.length(); i++){
+            if (max < h.get(word.charAt(i) - 'a')){
+                max = h.get(word.charAt(i) - 'a');
+            }
+        }
+        return max * word.length();
+    }
+
+}
+
+public class DesignerPDFViewer {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.boxSize(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
