c3c74186dd9da60d2cf34a92082672e2260830cd
KirtanManek <manekkirtan@gmail.com>
2022-09-23 23:49:01 +0530

    Create SherlockAndSquares.java

---
+++ HackerRank/SherlockAndSquares.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'squares' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER a
+     *  2. INTEGER b
+     */
+
+    public static int squares(int a, int b) {
+        return (int)(Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a)) + 1);
+        // Find floor value of square root of b.
+        // Find ceil value of square root of a.
+        // Subtract ceil value from floor value and add 1.
+        // Return above answer.
+    }
+
+}
+
+public class SherlockAndSquares {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int a = Integer.parseInt(firstMultipleInput[0]);
+
+                int b = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = Result.squares(a, b);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
