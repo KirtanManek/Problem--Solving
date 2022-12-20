40bce7df616e542f85873de1896858cbad66ff22
KirtanManek <manekkirtan@gmail.com>
2022-12-20 17:47:25 +0530

    Update PalindromeNumber.java

--- LeetCode/PalindromeNumber.java
+++ LeetCode/PalindromeNumber.java
@@ -1,17 +1,16 @@
+// Problem Link : https://leetcode.com/problems/palindrome-number
+
+
 class Solution {
     public boolean isPalindrome(int x) {
-        Palindrome p1 = new Palindrome();
-        p1.content = Integer.toString(x);
-        boolean ans = p1.checkPalindrome();
-        return ans;
-    }
-}
-class Palindrome{
-    String content;
-    String contentReverse = "";
-    boolean checkPalindrome() {
-        for (int i = content.length() - 1 ; i >= 0 ; i--) contentReverse += content.charAt(i);
-        if (contentReverse.equals(content)) return true;
-        else return false;
+        if(x < 0) return  false; 
+        int reversed = 0, remainder, original = x;
+        while(x != 0) {
+            remainder = x % 10; // reversed integer is stored in variable
+            reversed = reversed * 10 + remainder; //multiply reversed by 10 then add the remainder so it gets stored at next decimal place.
+            x  /= 10;  //the last digit is removed from num after division by 10.
+        }
+        // palindrome if original and reversed are equal
+        return original == reversed;
     }
 }
