6cc7d27bfd1919ff2a97281b4c2b122021f77ea3
KirtanManek <manekkirtan@gmail.com>
2022-12-23 21:07:49 +0530

    Create PlusOne.java

---
+++ LeetCode/PlusOne.java
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/plus-one/
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigit = new int [digits.length + 1];
+        newDigit[0] = 1;
+        return newDigit;
+    }
+}
