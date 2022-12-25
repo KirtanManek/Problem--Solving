a2003d14f9ce594313cdb31231dde5a05f3e1bc7
KirtanManek <manekkirtan@gmail.com>
2022-12-25 13:06:02 +0530

    Update Sqrt(x).java

--- LeetCode/Sqrt(x).java
+++ LeetCode/Sqrt(x).java
@@ -1,12 +1,12 @@
 // Problem Link : https://leetcode.com/problems/sqrtx
 
-// We can consider (√x-√x)2  = 0.
-// Replacing one of the √x‘s with y, then the equation becomes (y-√x)2 => y2 – 2y√x + x = 0
-// => √x = (y2 + x) / 2y
+// We can consider (√x-√x)^2  = 0.
+// Replacing one of the √x‘s with y, then the equation becomes (y-√x)^2 => y^2 – 2y√x + x = 0
+// => √x = (y^2 + x) / 2y
 // => √x = (y + x/y) / 2    We will use this formula.
 
 // In the above equation, we are considering √x as z.
-// So, to get a required decimal value we can compare the difference of y and z to 10-p 
+// So, to get a required decimal value we can compare the difference of y and z to 10^(-p) 
 // (to get the result upto 5 decimal digits, compare y-z to 10-5=0.00001). Until y-z exceeds it, the iteration continues.
 
 // As we want upto 0 decimal places, we will iterate till 1, as iterating till 0 results in infinite loop.
