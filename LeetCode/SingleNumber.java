40170df54ffee5b5f1653e500e532b92e7f6fa81
KirtanManek <manekkirtan@gmail.com>
2022-12-31 12:21:50 +0530

    Create SingleNumber.java

---
+++ LeetCode/SingleNumber.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int rem = 0;
+        for(int i = 0; i < nums.length; i++){
+            rem = rem ^ nums[i]; // Using XOR operation
+        }
+        return rem;
+    }
+}
