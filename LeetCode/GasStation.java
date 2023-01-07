6a49ec571585a8c96b406788f83b1caec23e3470
KirtanManek <manekkirtan@gmail.com>
2023-01-07 16:40:01 +0530

    Create GasStation.java

---
+++ LeetCode/GasStation.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0, totalCost = 0;
+        for(int i = 0 ; i < gas.length; i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if(totalGas < totalCost) return -1;
+        int remainsGas = 0, startIndex = 0;
+        for(int i = 0 ; i < gas.length; i++){
+            remainsGas = remainsGas + (gas[i] - cost[i]);
+            if(remainsGas < 0 ){
+                startIndex = i+1;
+                remainsGas = 0;
+            }
+        }
+        return startIndex;
+    }
+}
